b09cb62083ae2bb30c9118b2b07547040a4e121c
Michel Megens <mtm.megens@gmail.com>
2026-09-06 14:23:53 +0200

    Basic program

--- data-plane/src/main.c
+++ data-plane/src/main.c
@@ -7,9 +7,15 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+SEC("xdp")
 int xdp_main(struct xdp_md *ctx)
 {
+    __u32 packet_size;
+
+    packet_size = ctx->data_end - ctx->data;
+    bpf_printk("Packet received: size=%u\n", packet_size);
+
     return XDP_PASS;
 }
 
-char _license[] SEC("license") = "Dual GPL/BSD"; // NOLINT(readability-identifier-naming) -- libbpf loader convention (SEC("license"))
+char _license[] SEC("license") = "Dual BSD/GPL"; // NOLINT(readability-identifier-naming) -- libbpf loader convention (SEC("license"))
