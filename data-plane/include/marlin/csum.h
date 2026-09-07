98a42916cd2baad87504d75c7e93148954c98c7b
Michel Megens <mtm.megens@gmail.com>
2026-09-07 20:23:45 +0200

    Add headers

---
+++ data-plane/include/marlin/csum.h
@@ -0,0 +1,7 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause
+ *
+ * Checksum (CSUM) definitions and helpers.
+ */
+
+#pragma once
---
+++ data-plane/include/marlin/mtu.h
@@ -0,0 +1,7 @@
+/**
+ * SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause
+ *
+ * MTU (Maximum Transmission Unit) definitions and helpers.
+ */
+
+#pragma once
