7b6ecb4b17a873596df13bbb4952c2d9c55ecf5d
Michel Megens <mtm.megens@gmail.com>
2026-09-07 22:12:28 +0200

    Add encap files

---
+++ data-plane/src/gue.c
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause
+ *
+ * GUE encapsulation implementation.
+ */
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+#include <marlin/marlin.h>
+#include <marlin/encap.h>
+
+int marlin_gue_encap_packet(struct xdp_md *ctx, struct marlin_ctx *mctx)
+{
+    if(ctx == NULL || mctx == NULL) {
+        return MARLIN_ABORT_NULLREF;
+    }
+
+    return MARLIN_OK;
+}
